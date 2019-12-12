c769064b5b6a5a22b8e6634ad7932a9a1b76119e
antonio obregon <antonio.obregon@snoopconsulting.com>
2019-12-12 11:59:01 -0300

    se esta subiendo el Tablero de Control, con las modificaciones(titulo, barra de botones y el ajuste de las velocidades)

---
+++ snoopTableros/Controllers/TablerosController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using snoopTableros.Models;
+
+namespace snoopTableros.Controllers
+{
+    public class TablerosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+  
+    }
+}
